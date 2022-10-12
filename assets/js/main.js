8d9e4bac013dc8b766b6d42f68e785ca2f376558
Lukas Mann <lukas@lvmann.dk>
2022-10-12 10:02:57 +0200

    Added SW Script to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,28 @@ const date = new Date();
 const month = date.getMonth() + 1;
 const day = date.getDate();
 
+// Service worker
+const registerServiceWorker = async () => {
+	if ("serviceWorker" in navigator) {
+		try {
+			const registration = await navigator.serviceWorker.register("/sw.js", {
+				scope: "/",
+			});
+			if (registration.installing) {
+				console.log("Service worker installing");
+			} else if (registration.waiting) {
+				console.log("Service worker installed");
+			} else if (registration.active) {
+				console.log("Service worker active");
+			}
+		} catch (error) {
+			console.error(`Registration failed with ${error}`);
+		}
+	}
+};
+
+registerServiceWorker();
+
 // Global variables
 let openedDays = [];
 
